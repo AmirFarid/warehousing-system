05f13e3b3b36eea851e4c3f8f1ba0dec20214ee5
Amir Farid <amir.m.m.farid@gmail.com>
2020-07-09 23:32:14 +0430

    Init project.

---
+++ src/test/java/com/parchenegar/capsule/CapsuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parchenegar.capsule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapsuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
