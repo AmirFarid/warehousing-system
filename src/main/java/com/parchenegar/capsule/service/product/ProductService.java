2dad923a916d176165792e0ea3573b430abbaa6d
Amir Farid <amir.m.m.farid@gmail.com>
2022-01-17 18:38:05 +0430

    ProductService added.

---
+++ src/main/java/com/parchenegar/capsule/service/product/ProductService.java
@@ -0,0 +1,14 @@
+package com.parchenegar.capsule.service.product;
+
+
+import com.parchenegar.capsule.domain.product.Product;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService
+{
+    public Product save(Product product)
+    {
+        return product;
+    }
+}
