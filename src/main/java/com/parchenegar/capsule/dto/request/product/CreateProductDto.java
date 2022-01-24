1e69868e9bdf8829cc83084da87d38beecb5214e
Amir Farid <amir.m.m.farid@gmail.com>
2022-01-24 16:17:41 +0430

    Create product dto updated.

--- src/main/java/com/parchenegar/capsule/dto/request/product/CreateProductDto.java
+++ src/main/java/com/parchenegar/capsule/dto/request/product/CreateProductDto.java
@@ -1,6 +1,11 @@
 package com.parchenegar.capsule.dto.request.product;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.*;
 
 @Data
 @Builder
@@ -8,13 +13,40 @@ import lombok.*;
 @AllArgsConstructor
 public class CreateProductDto
 {
-    String name;
+    @NotNull
+    @NotEmpty
+    @JsonProperty("category_ids")
+    Long[] categoryIds;
+
+    @NotNull
+    @NotEmpty
+    @JsonProperty("product_type_id")
+    Long productTypeId;
+
+    @NotNull
+    @NotEmpty
+    @Min(2)
     String color;
-    String isActive;
-    String description;
-    String unitsOnOrder;
-    int qty;
+
+    @NotNull
+    @NotEmpty
+    @NegativeOrZero
+    Long qty;
+
+    @NotNull
+    @NotEmpty
+    @Min(2)
+    @JsonProperty("qty_unit")
     String qtyUnit;
-    long[] categoryIds;
-    long productTypeId;
+
+    @NotNull
+    @NotEmpty
+    @NegativeOrZero
+    String price;
+
+    @JsonIgnore
+    MultipartFile picture;
+
+    @JsonProperty("is_active")
+    Boolean isActive;
 }
