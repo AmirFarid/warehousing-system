87ea88145c8adbde76566650c318b7b80d72dcd9
Amir Farid <amir.m.m.farid@gmail.com>
2022-01-24 16:23:18 +0430

    Controller updated.

--- src/main/java/com/parchenegar/capsule/controller/product/ProductController.java
+++ src/main/java/com/parchenegar/capsule/controller/product/ProductController.java
@@ -1,22 +1,47 @@
 package com.parchenegar.capsule.controller.product;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.parchenegar.capsule.common.storage.StorageService;
 import com.parchenegar.capsule.controller.BaseRestController;
-import com.parchenegar.capsule.service.product.ProductService;
+import com.parchenegar.capsule.domain.product.Product;
+import com.parchenegar.capsule.dto.base.Response;
+import com.parchenegar.capsule.dto.request.product.CreateProductDto;
+import com.parchenegar.capsule.service.product.CreateProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
 
 
 @RestController
 @RequestMapping("/v1/product")
 public class ProductController extends BaseRestController
 {
-    private ProductService productService;
+    private CreateProductService createProductService;
 
     @Autowired
-    public ProductController(ProductService productService)
+    StorageService storageService;
+
+    @Autowired
+    public ProductController(CreateProductService createProductService)
     {
-        this.productService = productService;
+        this.createProductService = createProductService;
+    }
+
+    @PostMapping(value = "")
+    public ResponseEntity create(@Valid @RequestParam("product") String productString,
+                                 @RequestParam("picture") MultipartFile picture) throws Exception
+    {
+
+        CreateProductDto productDto = new ObjectMapper().readValue(productString, CreateProductDto.class);
+        productDto.setPicture(picture);
+        Product product = createProductService.create(productDto);
+
+        return Response.ok(product, translator.get("http.ok"));
     }
 
 }
+
