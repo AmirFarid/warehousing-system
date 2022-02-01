3be8ab31ec5d343483daec03a1c9f4364ed3d17b
Amir Farid <amir.m.m.farid@gmail.com>
2022-02-01 21:37:45 +0430

    Fix.

--- src/main/java/com/parchenegar/capsule/config/oauth/jwt/server/AuthorizationServerConfiguration.java
+++ src/main/java/com/parchenegar/capsule/config/oauth/jwt/server/AuthorizationServerConfiguration.java
@@ -1,21 +1,17 @@
 package com.parchenegar.capsule.config.oauth.jwt.server;
 
 import com.parchenegar.capsule.config.oauth.jwt.server.props.SecurityProperties;
-import com.parchenegar.capsule.service.UserDetailService;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
@@ -35,27 +31,11 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 @EnableConfigurationProperties(SecurityProperties.class)
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter
 {
-
     private final DataSource dataSource;
     private final PasswordEncoder passwordEncoder;
     private final SecurityProperties securityProperties;
     private final UserDetailsService userDetailsService;
     private final AuthenticationManager authenticationManager;
-
-    @Value("${jwt.clientId}")
-    private String clientId;
-
-    @Value("${jwt.client-secret}")
-    private String clientSecret;
-
-    @Value("${jwt.accessTokenValidititySeconds}")
-    private int accessTokenValiditySeconds;
-
-    @Value("${jwt.authorizedGrantTypes}")
-    private String[] authorizedGrantTypes;
-
-    @Value("${jwt.refreshTokenValiditySeconds}")
-    private int refreshTokenValiditySeconds;
 
     @Autowired
     public AuthorizationServerConfiguration(final DataSource dataSource,
